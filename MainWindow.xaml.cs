57a30bcbf626163dffeebcd86f2990bafea2fa0a
V-lira <v42719501@gmail.com>
2025-11-11 19:08:57 +0300

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public Person person = new Person();
+        public MainWindow()
+        {
+            InitializeComponent();
+            person = new Person { Name ="John", BirthDate = new DateTime(2009,3,8) };
+            this.DataContext = person;
+        }
+    }
+}
---
+++ Person.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp2
+{
+    public class Person :INotifyPropertyChanged
+    {
+        private string name;
+        private int age;
+        private DateTime birthDate = new DateTime(2000, 1,1);
+        public string Name
+        { get { return name; }
+            set 
+            { 
+                name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+        //public int Age
+        //{
+        //    get { return age; }
+        //    set { 
+        //        age = value;
+        //        OnPropertyChanged("Age"); }
+        //}
+        public DateTime BirthDate
+        {
+            get { return birthDate; }
+            set
+            {
+                birthDate = value;
+                OnPropertyChanged("BirthDate");
+                OnPropertyChanged("Age");
+            }
+        }
+        public int Age => DateTime.Now.Year - BirthDate.Year -
+                 (DateTime.Now.DayOfYear < BirthDate.DayOfYear ? 1 : 0);
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
